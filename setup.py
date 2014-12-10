270a2b6c967e9b18b4829f2845109c04ff200f95
makocchi-git <makocchi@gmail.com>
2014-12-10 18:04:17 +0900

    add long_description and classifiers

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 # -*- encodig: utf-8 -*-
 
+import os
+
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='blackbird-memcached',
@@ -12,7 +17,17 @@ setup(
     description=(
         'Get stats of memcached for blackbird by using "stats".'
     ),
+    long_description=read('PROJECT.txt'),
+    classifiers=[
+      'Development Status :: 4 - Beta',
+      'Programming Language :: Python :: 2',
+      'Programming Language :: Python :: 2.6',
+    ],
     author='ARASHI, Jumpei',
     author_email='jumpei.arashi@arashike.com',
     url='https://github.com/Vagrants/blackbird-memcached',
+    data_files=[
+        ('/opt/blackbird/plugins', ['memcached.py']),
+        ('/etc/blackbird/conf.d', ['memcached.cfg'])
+    ],
 )
